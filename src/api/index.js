16cdb15552d0d4bedf5dd566dfe0c99c3b796af1
wangmeng <1793584681@qq.com>
2022-04-04 21:54:16 +0800

    20220404

---
+++ src/api/index.js
@@ -0,0 +1,3 @@
+export { default as trademark } from "./product/trademark";
+export { default as attr } from "./product/attr";
+export { default as category } from "./product/category";
---
+++ src/views/product/sku/List.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>SkuList</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
